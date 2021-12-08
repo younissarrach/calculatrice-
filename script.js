41acb19a3a203b4151c1ae9492a011d95d57c943
younissarrach <youniisarrach@gmail.com>
2021-12-08 13:24:36 +0100

    calcul in progress

---
+++ script.js
@@ -0,0 +1,65 @@
+// const numberbuttons = document.querySelectorAll('[data-number]')
+// const operationbuttons = document.querySelectorAll('[data-operation]')
+// const equalbuttons = document.querySelectorAll('[data-equal]')
+// const deletebuttons = document.querySelectorAll('[data-delete]')
+
+const equal = document.getElementById('equal')
+equal.addEventListener('click', function() {
+    alert('wa chrif rak brk 3la equal')
+});
+
+// a.value
+const one = document.getElementById('one');
+one.addEventListener('click', function() {
+    inp.value = 1
+});
+const two = document.getElementById('two');
+two.addEventListener('click', function() {
+    inp.value = 2
+});
+const three = document.getElementById('three');
+three.addEventListener('click', function() {
+    inp.value = 3
+});
+const four = document.getElementById('four');
+four.addEventListener('click', function() {
+    inp.value = 4
+});
+const five = document.getElementById('five');
+five.addEventListener('click', function() {
+    inp.value = 5
+});
+const six = document.getElementById('six');
+one.addEventListener('click', function() {
+    inp.value = 6
+});
+const seven = document.getElementById('seven');
+seven.addEventListener('click', function() {
+    inp.value = 7
+});
+const eight = document.getElementById('eight')
+eight.addEventListener('click', function() {
+    inp.value = 8
+});
+const nine = document.getElementById('nine')
+nine.addEventListener('click', function() {
+    inp.value = 9
+});
+const inp = document.getElementById('current-operand');
+inp.value = 4444
+const addition = document.getElementById('addition');
+addition.addEventListener('click', function() {
+    inp.value = 11
+});
+const division = document.getElementById('division');
+division.addEventListener('click', function() {
+    inp.value = 0
+});
+const subtraction = document.getElementById('subtraction')
+subtraction.addEventListener('click', function() {
+    inp.value = 30
+});
+const Multiiplication = document.getElementById('Multiplication')
+Multiiplication.addEventListener('click', function() {
+    inp.value = 50
+});
